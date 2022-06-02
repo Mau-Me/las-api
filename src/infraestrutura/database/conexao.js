de295a60b89c51b2c05561280868e0c8b855b5cb
Mau-Me <mauriciosm95@gmail.com>
2022-06-02 09:27:30 -0300

    Update: Add conexão com bd heroku

--- src/infraestrutura/database/conexao.js
+++ src/infraestrutura/database/conexao.js
@@ -1,18 +1,19 @@
 const mysql = require("mysql");
-
 const DBHOST = process.env.DBHOST;
 const DBPORT = process.env.DBPORT;
 const DBUSER = process.env.DBUSER;
 const DBPASS = process.env.DBPASS;
 const DBNAME = process.env.DBNAME;
 
-const pool = mysql.createPool({
+const localConfig = {
   connectionLimit: 100,
   host: DBHOST || "localhost",
   port: DBPORT || 3306,
   user: DBUSER || "las",
   password: DBPASS || "admin",
   database: DBNAME || "las",
-});
+};
+
+const pool = mysql.createPool(process.env.DATABASE_URL || localConfig);
 
 module.exports = pool;
