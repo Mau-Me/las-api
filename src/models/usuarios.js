a9a0be13fd61ab0369bb106f8c56dd2c74b01d9f
Mau-Me <mauriciosm95@gmail.com>
2022-06-02 11:14:12 -0300

    Fix: Code smell regex

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -108,7 +108,7 @@ class Usuarios {
   async validarURLFotoPerfil(url) {
     try {
       const regex =
-        /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+        /https?:\/\/(www.)?[-a-zA-Z0-9@:%.+~#=]{1,256}.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%+.~#?&//=]*)/gm;
       const verificaUrl = url.match(regex);
       if (!verificaUrl) {
         return false;
